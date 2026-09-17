a9dd0c86b884bdb47aa3aed4a532ae7f9427293e
Josh Rees <josh@lumopep.com>
2026-09-17 13:11:18 -0700

    feat: add Omnisend tracking snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,12 @@ export default function RootLayout({
         <FloatingCartButton />
         <Toast />
         <PopupManager />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `window.omnisend = window.omnisend || []; omnisend.push(["brandID", "6aac47acf65e46dae6b9a464"]); omnisend.push(["track", "$pageViewed"]); !function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://omnisnippet1.com/inshop/launcher-v2.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)}();`
+          }}
+        />
       </body>
     </html>
   );
